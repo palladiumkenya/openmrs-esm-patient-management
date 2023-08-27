3b201049c6363cd6305c98215562c901f3423829
Ayush <54752747+ayush-AI@users.noreply.github.com>
2023-08-28 00:31:27 +0530

    (test): Add test for `patient-list-detail.component` (#790)

--- packages/esm-patient-registration-app/src/patient-registration/patient-registration.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/patient-registration.component.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import classNames from 'classnames';
-import { Button, InlineLoading, Link } from '@carbon/react';
-import { XAxis } from '@carbon/react/icons';
+import { Button, Link, InlineLoading } from '@carbon/react';
+import { XAxis, ShareKnowledge } from '@carbon/react/icons';
 import { useLocation, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Form, Formik, type FormikHelpers } from 'formik';
@@ -31,6 +31,8 @@ import { builtInSections, type RegistrationConfig, type SectionDefinition } from
 import { SectionWrapper } from './section/section-wrapper.component';
 import BeforeSavePrompt from './before-save-prompt';
 import styles from './patient-registration.scss';
+import PatientVerification from '../patient-verification/patient-verification.component';
+import { handleSavePatientToClientRegistry } from '../patient-verification/patient-verification-hook';
 
 let exportedInitialFormValuesForTesting = {} as FormValues;
 
@@ -170,7 +172,7 @@ export const PatientRegistration: React.FC<PatientRegistrationProps> = ({ savePa
 
   return (
     <Formik
-      enableReinitialize
+      enableReinitialize={true}
       initialValues={initialFormValues}
       validationSchema={validationSchema}
       onSubmit={onFormSubmit}>
@@ -194,6 +196,17 @@ export const PatientRegistration: React.FC<PatientRegistrationProps> = ({ savePa
                     </Link>
                   </div>
                 ))}
+                <Button
+                  renderIcon={ShareKnowledge}
+                  disabled={!currentSession || !identifierTypes}
+                  onClick={() => {
+                    props.isValid
+                      ? handleSavePatientToClientRegistry(props.values, props.setValues)
+                      : props.validateForm().then((errors) => displayErrors(errors));
+                  }}
+                  className={styles.submitButton}>
+                  {t('postToRegistry', 'Post to registry')}
+                </Button>
                 <Button
                   className={styles.submitButton}
                   type="submit"
@@ -234,6 +247,7 @@ export const PatientRegistration: React.FC<PatientRegistrationProps> = ({ savePa
                   initialFormValues: props.initialValues,
                   setInitialFormValues,
                 }}>
+                <PatientVerification props={props} />
                 {sections.map((section, index) => (
                   <SectionWrapper
                     key={`registration-section-${section.id}`}
