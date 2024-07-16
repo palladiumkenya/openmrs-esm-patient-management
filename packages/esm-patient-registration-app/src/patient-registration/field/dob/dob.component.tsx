14afb6822949fe8868748450db816875e086b768
makombe <kennedymakombe@gmail.com>
2024-07-16 17:30:11 +0300

    Fix dob date picker increasing selected date by a month

--- packages/esm-patient-registration-app/src/patient-registration/field/dob/dob.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/dob/dob.component.tsx
@@ -46,7 +46,7 @@ export const DobField: React.FC = () => {
 
   const onDateChange = useCallback(
     (birthdate: CalendarDate) => {
-      setFieldValue('birthdate', new Date(birthdate.year, birthdate.month, birthdate.day, 0, 0, 0, 0));
+      setFieldValue('birthdate', birthdate?.toDate(getLocalTimeZone()));
     },
     [setFieldValue],
   );
