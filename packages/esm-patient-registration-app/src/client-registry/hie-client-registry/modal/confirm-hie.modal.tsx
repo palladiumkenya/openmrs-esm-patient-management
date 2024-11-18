cd8a52130e0c073cae4526bdbd7ea0a90bb7d6f5
its-kios09 <legacyitsolution@gmail.com>
2024-11-18 12:26:38 +0300

    (feat) added dependant view UI

--- packages/esm-patient-registration-app/src/client-registry/hie-client-registry/modal/confirm-hie.modal.tsx
+++ packages/esm-patient-registration-app/src/client-registry/hie-client-registry/modal/confirm-hie.modal.tsx
@@ -1,18 +1,60 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, ModalBody, ModalHeader, ModalFooter, Accordion, AccordionItem, CodeSnippet } from '@carbon/react';
-import styles from './confirm-hie.scss';
 import { age, ExtensionSlot, formatDate } from '@openmrs/esm-framework';
 import { type HIEPatient } from '../hie-types';
 import capitalize from 'lodash-es/capitalize';
+import styles from './confirm-hie.scss';
 
 const PatientInfo: React.FC<{ label: string; value: string }> = ({ label, value }) => {
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '0.25fr 0.75fr', margin: '0.25rem' }}>
-      <span style={{ minWidth: '5rem', fontWeight: 'bold' }}>{label}</span>
-      <span>{value}</span>
+    <div className={styles.patientInfo}>
+      <span className={styles.patientInfoLabel}>{label}</span>
+      <span>{value || '--'}</span>
     </div>
   );
+};
+
+const DependentInfo: React.FC<{ dependents: any[] }> = ({ dependents }) => {
+  const { t } = useTranslation();
+
+  if (dependents && dependents.length > 0) {
+    return (
+      <div>
+        <span className={styles.header}>{t('dependants', 'Dependants')}</span>
+        {dependents.map((dependent, index) => {
+          const name = dependent?.name?.text;
+          const relationship =
+            dependent?.relationship?.[0]?.coding?.[0]?.display || t('unknownRelationship', 'Unknown');
+          const nationalID = dependent?.extension?.find(
+            (ext) =>
+              ext.url === 'http://cr.tiberbu.app/fhir/StructureDefinition/dependants-id-number' &&
+              ext.valueIdentifier?.type?.coding?.[0]?.code === 'national-id',
+          )?.valueIdentifier?.value;
+          const birthCertificate = dependent?.extension?.find(
+            (ext) =>
+              ext.url === 'http://cr.tiberbu.app/fhir/StructureDefinition/dependants-id-number' &&
+              ext.valueIdentifier?.type?.coding?.[0]?.code === 'birth-certificate-number',
+          )?.valueIdentifier?.value;
+
+          const primaryIdentifier = nationalID || birthCertificate;
+          const identifierLabel = nationalID
+            ? t('nationalID', 'National ID')
+            : t('birthCertificate', 'Birth Certificate');
+
+          return (
+            <div key={index} className={styles.dependentInfo}>
+              <PatientInfo label={t('name', 'Name')} value={name} />
+              <PatientInfo label={t('relationship', 'Relationship')} value={relationship} />
+              <PatientInfo label={identifierLabel} value={primaryIdentifier} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return null;
 };
 
 interface HIEConfirmationModalProps {
@@ -23,8 +65,8 @@ interface HIEConfirmationModalProps {
 
 const HIEConfirmationModal: React.FC<HIEConfirmationModalProps> = ({ closeModal, patient, onUseValues }) => {
   const { t } = useTranslation();
-  const firstName = patient?.name[0]['given']?.[0];
-  const lastName = patient?.name[0]['family'];
+  const firstName = patient?.name[0]?.given?.[0];
+  const lastName = patient?.name[0]?.family;
 
   const handleUseValues = () => {
     onUseValues();
@@ -37,13 +79,13 @@ const HIEConfirmationModal: React.FC<HIEConfirmationModalProps> = ({ closeModal,
         <span className={styles.header}>{t('hieModal', 'HIE Patient Record Found')}</span>
       </ModalHeader>
       <ModalBody>
-        <div style={{ display: 'flex', margin: '1rem' }}>
+        <div className={styles.patientDetails}>
           <ExtensionSlot
-            style={{ display: 'flex', alignItems: 'center' }}
+            className={styles.patientPhotoContainer}
             name="patient-photo-slot"
             state={{ patientName: `${firstName} ${lastName}` }}
           />
-          <div style={{ width: '100%', marginLeft: '0.625rem' }}>
+          <div className={styles.patientInfoContainer}>
             <PatientInfo label={t('healthID', 'HealthID')} value={patient?.id} />
             <PatientInfo label={t('patientName', 'Patient name')} value={`${firstName} ${lastName}`} />
             <PatientInfo label={t('age', 'Age')} value={age(patient?.birthDate)} />
@@ -53,9 +95,15 @@ const HIEConfirmationModal: React.FC<HIEConfirmationModalProps> = ({ closeModal,
               label={t('maritalStatus', 'Marital status')}
               value={patient?.maritalStatus?.coding?.map((m) => m.code).join('')}
             />
-            <PatientInfo label={t('dependents', 'Dependents')} value="--" />
+
+            {(!patient?.contact || patient?.contact.length === 0) && (
+              <PatientInfo label={t('dependents', 'Dependents')} value="--" />
+            )}
           </div>
         </div>
+
+        <DependentInfo dependents={patient?.contact} />
+
         <div>
           <Accordion>
             <AccordionItem title={t('viewFullResponse', 'View full response')}>
