32957a9080ab4e72cb00b7c8ec9921ae69df0c63
Fredrick Kioko <67967749+its-kios09@users.noreply.github.com>
2024-11-25 15:34:27 +0300

    (fix) fixed missing dependants on some national IDs (#56)
    * (fix) fixed missing dependants on some national IDs
    
    * (fix) fixed merge conflicts

--- packages/esm-patient-registration-app/src/client-registry/hie-client-registry/hie-types.ts
+++ packages/esm-patient-registration-app/src/client-registry/hie-client-registry/hie-types.ts
@@ -5,6 +5,106 @@ export type HIEPatient = fhir.Patient & {
   }>;
 };
 
+export interface HIEPatientResponse {
+  id: string;
+  meta: Metadata;
+  link: Link[];
+  entry: Entry[];
+}
+
+interface Metadata {
+  lastUpdated: string;
+}
+
+interface Link {
+  relation: string;
+  url: string;
+}
+
+interface Entry {
+  resource: Resource;
+}
+
+interface Resource {
+  id: string;
+  extension: Extension[];
+  identifier: Identifier[];
+  active: boolean;
+  name: Name[];
+  telecom: Telecom[];
+  birthDate: string;
+  address: Address[];
+  gender: string;
+  maritalStatus: MaritalStatus;
+  contact: Contact[];
+}
+
+interface Extension {
+  url: string;
+  valueString: string;
+}
+
+interface Identifier {
+  type: CodingType;
+  value: string;
+}
+
+interface CodingType {
+  coding: Coding[];
+}
+
+interface Coding {
+  system?: string;
+  code: string;
+  display: string;
+}
+
+interface Name {
+  text: string;
+  family: string;
+  given: string[];
+}
+
+interface Telecom {
+  system: string;
+  value?: string;
+}
+
+interface Address {
+  extension: AddressExtension[];
+  city: string;
+  country: string;
+}
+
+interface AddressExtension {
+  url: string;
+  valueString: string;
+}
+
+interface MaritalStatus {
+  coding: Coding[];
+}
+
+interface Contact {
+  id: string;
+  extension: ContactExtension[];
+  relationship: Relationship[];
+  name: Name;
+  telecom: Telecom[];
+  address: Address;
+  gender: string;
+}
+
+interface ContactExtension {
+  url: string;
+  valueIdentifier?: Identifier;
+  valueString?: string;
+}
+
+interface Relationship {
+  coding: Coding[];
+}
+
 export type APIClientConfig = {
   baseUrl: string;
   credentials: string;
